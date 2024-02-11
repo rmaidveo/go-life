88f5c6e8c97a64b6de76116389018641ed30a0ef
rmaidveo <ilonazelenaya@gmail.com>
2024-02-11 21:25:40 +0300

    Finalize tha main code

--- cmd/go-life/main.go
+++ cmd/go-life/main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"github.com/rmaidveo/go-life"
 )
 
 func main() {
-	const text = ".....\n.....\n.OOO.\n.....\n....."
-	const outDelay = 100 * time.Millisecond
+	outDelay := flag.Duration("delay", 100*time.Millisecond, "delay between frames")
+	flag.Parse()
 
-	field, err := life.ParseField(text)
+	text, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("unable to read the field: %s", err)
+	}
+
+	field, err := life.ParseField(string(text))
 	if err != nil {
 		log.Fatalf("unable to parse the field: %s", err)
 	}
 
 	for {
 		fmt.Print(field)
-		time.Sleep(outDelay)
+		time.Sleep(*outDelay)
 
 		field = field.NextField()
 	}
